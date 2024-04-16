84e5ada17c12a3b1f4003e477d45613539049300
Rachid KABBAB <r.kabbab@gmail.com>
2024-04-16 22:51:56 +0000

    Adding all other files I tested on my local machine

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * It creates a set from an array
+ * @param {Array} array - The source array
+ * @author Rachid KABBAB
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
