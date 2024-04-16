4ff42ccd3efa402948899d4b54c49d83a38c022c
Rachid KABBAB <r.kabbab@gmail.com>
2024-04-16 22:24:05 +0000

    0 Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * It retrieves a list of students
+ * @author Rachid KABBAB
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
