e41a1801a62b0eded98a8195dc2a30d0c7002d5f
Rachid KABBAB <r.kabbab@gmail.com>
2024-06-04 23:06:18 +0000

    New file

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Rachid KABBAB !
+ * @author Rachid KABBAB <https://github.com/karachid>
  */
 const displayMessage = (msg) => {
   console.log(msg);
